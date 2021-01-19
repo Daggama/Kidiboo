a2ed942c93e15eb7b571dc45e7ca5b0e8d941c9b
URAZOVDAULET <imdauleturazov@mail.ru>
2021-01-19 16:33:54 +0600

    dobavil modeli

--- TEDEN/models.py
+++ TEDEN/models.py
@@ -1,20 +1,30 @@
 from django.db import models
-
+from django.urls import reverse
+import uuid
 # Create your models here.
 
+class Genre(models.Model):
+    
+    name = models.CharField(max_length=200, help_text="Enter a book genre (e.g. Science Fiction, French Poetry etc.)")
+
+    def __str__(self):
+
+        return self.name
 
 class Book(models.Model):
 
-    title = models.CharField(max_length=50)
-    genre = models.ForeignKey('Genre', on_delete=models.SET_NULL, null=True)
+    title = models.CharField(max_length=200)
+    genre = models.ManyToManyField(Genre, help_text="выбирите жанр для этой книги")
     author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True)
     language = models.ForeignKey('Language', on_delete=models.SET_NULL, null=True)
     image = models.ImageField()
-    description = models.TextField(max_length=1000)
-
-    def str(self):
+    description = models.TextField(max_length=1000, help_text="Опишите краткое содержание книги")
+    
+    def __str__(self):
         return self.title
 
+    def get_absolute_url(self):
+        return reverse('book-detail', args=[str(self.id)])
 
 class Author(models.Model):
 
@@ -23,7 +33,7 @@ class Author(models.Model):
     date_of_birth = models.DateField(null=True, blank=True)
     date_of_death = models.DateField('died', null=True, blank=True)
 
-    def str(self):
+    def __str__(self):
         return '%s, %s' % (self.last_name, self.first_name)
 
 
@@ -31,13 +41,31 @@ class Language(models.Model):
 
     name = models.CharField(max_length=200)
 
-    def str(self):
+    def __str__(self):
         return self.name
 
 
-class Genre(models.Model):
 
-    name = models.CharField(max_length=50)
 
-    def str(self):
-        return self.name
+class BookInstance(models.Model):
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text="Unique ID for this particular book across whole library")
+    book = models.ForeignKey('Book', on_delete=models.SET_NULL, null=True)
+    imprint = models.CharField(max_length=200)
+    due_back = models.DateField(null=True, blank=True)
+
+    LOAN_STATUS = (
+        ('m', 'Maintenance'),
+        ('o', 'On loan'),
+        ('a', 'Available'),
+        ('r', 'Reserved'),
+    )
+
+    status = models.CharField(max_length=1, choices=LOAN_STATUS, blank=True, default='m', help_text='Book availability')
+
+    class Meta:
+        ordering = ["due_back"]
+
+
+    def __str__(self):
+        return '%s (%s)' % (self.id,self.book.title)
